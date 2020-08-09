6e549b97fce68a2c51225c03951b6cb34e8cf542
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-08-09 15:36:45 +0100

    userdata seems working

--- launch_instance.py
+++ launch_instance.py
@@ -23,27 +23,8 @@ response = client.run_instances(
         'sg-08ee4c5336b8dcba4',
     ],
     SubnetId='subnet-0ec7bfc49d905e146',
-    UserData='#!/bin/bash'
-             'sudo yum update -y amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2'
-             'sudo yum install -y httpd mariadb-server'
-             'sudo systemctl start httpd'
-             'sudo systemctl enable httpd'
-             'sudo usermod -a -G apache ec2-user'
-             'sudo chown -R ec2-user:apache /var/www'
-             'sudo chmod 2775 /var/www'
-             'sudo find /var/www -type d -exec chmod 2775 {} \;'
-             'sudo find /var/www -type f -exec chmod 0664 {} \;'
-             'sudo echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php',
+    UserData=open("/home/pi-ux-ce/git/aws_infrastructure/userdata.sh").read(),
     InstanceInitiatedShutdownBehavior='terminate',
-    InstanceMarketOptions={
-            'MarketType': 'spot',
-            'SpotOptions': {
-                'MaxPrice': '0.003',
-                'SpotInstanceType': 'one-time',
-                'BlockDurationMinutes': 60,
-                'InstanceInterruptionBehavior': 'terminate'
-            }
-    },
     TagSpecifications=[
         {
             'ResourceType': 'instance',
