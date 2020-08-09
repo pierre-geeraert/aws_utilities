9328b31ec4fe28d454de63e80952264b771c3d58
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-08-09 23:03:37 +0100

    modify the beauty of the UI

--- display_instances.py
+++ display_instances.py
@@ -2,13 +2,19 @@ import boto3
 import json
 import pprint
 
+Number_of_instances=0
+
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
+        Number_of_instances=Number_of_instances+1
+        print("=======================================================")
+        print("Instance number "+str(Number_of_instances))
+        print("=======================================================")
         # This sample print will output entire Dictionary object
-        pprint.pprint(instance)
+        #pprint.pprint(instance)
         # This will print will output the value of the Dictionary key 'InstanceId'
-        print(instance["InstanceId"])
-
+        print("Instance ID: "+str(instance["InstanceId"]))
+        print("Instance Name: "+str(instance["Tags"]))
 
