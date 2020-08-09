1436b45cfea3edb6ac7feaddeaebfc7f0b65fc70
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-08-09 15:21:29 +0100

    manage to run UserData

--- display_instances.py
+++ display_instances.py
@@ -1,11 +1,13 @@
 import boto3
+import json
+import pprint
 
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
         # This sample print will output entire Dictionary object
-        #print(instance)
+        pprint.pprint(instance)
         # This will print will output the value of the Dictionary key 'InstanceId'
         print(instance["InstanceId"])
 
--- launch_instance.py
+++ launch_instance.py
@@ -1,3 +1,6 @@
+import pprint
+from datetime import datetime
+
 import boto3
 client = boto3.client('ec2')
 
@@ -7,30 +10,55 @@ response = client.run_instances(
         {
             'DeviceName': '/dev/sdh',
             'Ebs': {
-                'VolumeSize': 100,
+                'VolumeSize': 20,
             },
         },
     ],
     ImageId='ami-07d9160fa81ccffb5',
-    InstanceType='t2.micro',
+    InstanceType='t3a.nano',
     KeyName='Debian_buster_kp',
     MaxCount=1,
     MinCount=1,
     SecurityGroupIds=[
-        'sg-0be7eaf90b053653b',
+        'sg-08ee4c5336b8dcba4',
     ],
-    SubnetId='subnet-06592162726fc29ed',
+    SubnetId='subnet-0ec7bfc49d905e146',
+    UserData='#!/bin/bash'
+             'sudo yum update -y amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2'
+             'sudo yum install -y httpd mariadb-server'
+             'sudo systemctl start httpd'
+             'sudo systemctl enable httpd'
+             'sudo usermod -a -G apache ec2-user'
+             'sudo chown -R ec2-user:apache /var/www'
+             'sudo chmod 2775 /var/www'
+             'sudo find /var/www -type d -exec chmod 2775 {} \;'
+             'sudo find /var/www -type f -exec chmod 0664 {} \;'
+             'sudo echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php',
+    InstanceInitiatedShutdownBehavior='terminate',
+    InstanceMarketOptions={
+            'MarketType': 'spot',
+            'SpotOptions': {
+                'MaxPrice': '0.003',
+                'SpotInstanceType': 'one-time',
+                'BlockDurationMinutes': 60,
+                'InstanceInterruptionBehavior': 'terminate'
+            }
+    },
     TagSpecifications=[
         {
             'ResourceType': 'instance',
             'Tags': [
                 {
-                    'Key': 'Purpose',
+                    'Key': 'Name',
                     'Value': 'test',
+                },
+{
+                    'Key': 'development',
+                    'Value': 'true',
                 },
             ],
         },
     ],
 )
 
-print(response)
+pprint.pprint(response)
